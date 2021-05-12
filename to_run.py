24b976c5f810b4833d8f8cfb4bcd2d69a0d78a4a
Ze-Falcon <73237463+Ze-Falcon@users.noreply.github.com>
2021-05-12 15:09:38 +0545

    Update to_run.py

--- to_run.py
+++ to_run.py
@@ -3,10 +3,9 @@ import schedule
 
 def bypass_ses():
     os.system("python ./bypass.py")
-    print("Bypassing ..")
+    print("Startinf Bypass ..")
 
-#schedule.every(2.5).hours.do(bypass_ses())
-schedule.every(2.5).minutes.do(bypass_ses)
+schedule.every(2.5).hours.do(bypass_ses)
 
 while 1:
     schedule.run_pending()
